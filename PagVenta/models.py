d06a69e28dad5da3428e542180df0c3344733f8c
Totobi :D <mrtotobi@gmail.com>
2019-10-30 22:03:21 -0300

    Creado models de Producto

--- PagVenta/models.py
+++ PagVenta/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+import get
 
-# Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length=5)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    comentario = models.TextField()
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+    
