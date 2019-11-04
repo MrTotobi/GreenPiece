713c65525e3c3d3b4a6ecf281012dc7a0f353d09
Totobi :D <mrtotobi@gmail.com>
2019-11-04 11:43:08 -0300

    agregada autenticacion

--- PagVenta/models.py
+++ PagVenta/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-import get
 
 class Producto(models.Model):
     codigo = models.CharField(max_length=5)
@@ -13,4 +12,8 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre
 
-    
+    class Meta:
+        permissions = (
+            ('admin',('Es admin')),
+            ('cliente',('Es cliente')),
+        )
