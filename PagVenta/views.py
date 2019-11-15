d1132ee2fc5a3510335d8dd35bcf64025b4d6a77
michu <michuekaterina@gmail.com>
2019-11-15 12:38:13 -0300

    se hizo el CRUD :)

--- PagVenta/views.py
+++ PagVenta/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django import forms
+#-----importamos nuestras estructuras-------------------
+from .models  import Producto
+from .forms import ProductoForm
+
 
 def index(request):
     return render(request,'app/index.html')
@@ -11,3 +19,39 @@ def juegos_mesa(request):
 
 def formulario(request):
     return render(request,'app/formulario.html')
+
+#----CREAMOS UNA FUNCIUON PARA AGREGAR CARRERA----
+
+def inscribir_producto(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if(form.is_valid):
+            model_instance = form.save(commit=False)
+            model_instance.save()
+            return redirect('/addProducto')
+    else:
+        form = ProductoForm()
+        return render(request, "app/ins_carrera.html",{'form':form})
+
+def listar_producto(request):
+    Producto = Producto.objects.all()
+    return render(request,"app/listar_prducto.html"),{'producto':Producto}
+
+def editar_producto(request, codigo):
+    instancia = Producto.objects.get(id=codigo)
+    form = ProductoForm(instance=instancia)
+
+    if request.method == "POST":
+        form = ProductoForm(request.POST, instance= instancia)
+        if form.is_valid():
+            instancia= form.save(commit=False)
+            instancia.save()
+
+    return render(request, "app/editar_producto.html",{'form':form})
+
+def borrar_producto(request, codigo):
+    instancia = Producto.objects.get(id=codigo)
+    instancia.delete()
+    return redirect("/")
+
+    
